d2f5a7dbf83c5e4348529cf7d9cffcbeb2e8f622
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-11-24 15:37:36 -0800

    updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,30 +2,21 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 const config: Config = {
   title: "GunKustom",
   tagline: "Internal Docs Server",
   favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
+  // Set the production URL of your site here
   url: "https://gunkustom.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/GunKustom-docs-internal/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "gunkustom", // Usually your GitHub org/user name.
-  projectName: "GunKustom-docs-internal", // Usually your repo name.
+  organizationName: "gunkustom",
+  projectName: "GunKustom-docs-internal",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -37,10 +28,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/gunkustom/GunKustom-docs-internal/tree/main/",
         },
         blog: {
           showReadingTime: true,
@@ -48,11 +37,8 @@ const config: Config = {
             type: ["rss", "atom"],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
+            "https://github.com/gunkustom/GunKustom-docs-internal/tree/main/",
           onInlineTags: "warn",
           onInlineAuthors: "warn",
           onUntruncatedBlogPosts: "warn",
@@ -65,7 +51,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
       title: "GunKustom",
@@ -91,15 +76,7 @@ const config: Config = {
     footer: {
       style: "dark",
       links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Docs",
-              to: "/docs/intro",
-            },
-          ],
-        },
+        { title: "Docs", items: [{ label: "Docs", to: "/docs/intro" }] },
         {
           title: "Community",
           items: [
@@ -111,19 +88,13 @@ const config: Config = {
               label: "Discord",
               href: "https://discordapp.com/invite/docusaurus",
             },
-            {
-              label: "X",
-              href: "https://x.com/docusaurus",
-            },
+            { label: "X", href: "https://x.com/docusaurus" },
           ],
         },
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
+            { label: "Blog", to: "/blog" },
             {
               label: "GitHub",
               href: "https://github.com/gunkustom/GunKustom-docs-internal",
